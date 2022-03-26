8e1ab1b7a42245196a5b2fbfcd36b078c900c700
jutaics <jutaics@hotmail.com>
2022-03-26 20:56:37 -0300

    Refactoring do projeto prático portal de notícias com Consign (routes)

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var app = require('./config/server');
 
-var rotasNoticias = require('./app/routes/noticias')(app);
+//var rotasNoticias = require('./app/routes/noticias')(app);
 
-var rotaHome = require('./app/routes/home')(app);
+//var rotaHome = require('./app/routes/home')(app);
 
-var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
+//var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function () {
     console.log("Servidor ON");
