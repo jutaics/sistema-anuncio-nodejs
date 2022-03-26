dcf7052b9a4744b62b7cb2533773f4c202cb4e06
jutaics <jutaics@hotmail.com>
2022-03-26 20:47:19 -0300

    Refactoring do método de conexão com o banco de dados MySQL

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,17 +1,10 @@
+var dbConnection = require('../../config/dbConnection');
 const res = require('express/lib/response');
 
 module.exports = function(app){
 
-    app.get('/noticias', function(req, res){
-
-        var mysql = require('mysql');
-
-        var connection = mysql.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password : '1234',
-        database : 'portal_noticias'
-        });
+    var connection = dbConnection();
+    app.get('/noticias', function(req, res){     
    
         connection.query('select * from noticias', function(erro, result){
             //res.send(result);
