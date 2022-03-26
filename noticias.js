d474bbd8e974abff2f422a938a572ef34c11d4a3
jutaics <jutaics@hotmail.com>
2022-03-26 14:06:16 -0300

    Respondendo requisições com base na url requisitada

--- noticias.js
+++ noticias.js
@@ -1,7 +1,21 @@
 var http = require('http');
 
-var server = http.createServer( function(req, res){
-res.end("<html><body>Portal de Notícias </body></html>");
+var server = http.createServer(function (req, res) {
+
+    var categoria = req.url;
+
+    if (categoria == '/tecnologia') {
+        res.end("<html><body>Notíocias de Tecnologia</body></html>");
+    }
+    else if (categoria == '/moda') {
+        res.end("<html><body>Notíocias de Moda</body></html>");
+    }
+    else if (categoria == '/beleza') {
+        res.end("<html><body>Notíocias de Beleza</body></html>");
+    }
+    else {
+        res.end("<html><body>Portal de Notícias </body></html>");
+    }
 });
 
 server.listen(3000);
