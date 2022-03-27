8a55cec855fd59254737c8a37a8b45f4b02d5aa5
jutaics <jutaics@hotmail.com>
2022-03-26 21:56:32 -0300

    Inserindo registros no banco de dados MySQL

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,11 +1,18 @@
-module.exports = function(app){
+module.exports = function(application){
 
-    app.get('/formulario_inclusao_noticia', function (req, res) {
+    application.get('/formulario_inclusao_noticia', function (req, res) {
         res.render("admin/form_add_noticia");
     });
 
-    app.post('/noticias/salvar', function (req, res) {
-        var noticias = req.body;
-        res.send(noticias);
+    application.post('/noticias/salvar', function (req, res) {
+        var noticia = req.body;
+        
+        var connection = application.config.dbConnection();
+        var noticiasModel = application.app.models.noticiasModel;
+   
+        noticiasModel.salvarNoticia(noticia, connection, function(error, result){                  
+         res.redirect('/noticias');
+        }); 
+
     });
 };
